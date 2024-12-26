843d957e9391c4576314d18baeda882cc79f7c12
AntonC9018 <anton.curm@gmail.com>
2024-12-26 12:40:30 +0200

    Reformat js

--- src/extension/background.js
+++ src/extension/background.js
@@ -3,12 +3,8 @@ rerun();
 function rerun()
 {
     let port = chrome.runtime.connectNative('unpack_zip');
-    port.onMessage.addListener(/** @param {string} message */ message =>
+    port.onMessage.addListener(message =>
     {
-        // let div = document.getElementById("errors");
-        // let row = document.createElement("div");
-        // row.textContent = message;
-        // div.append(row);
         if (typeof(message) == "string")
         {
             console.log(message);
@@ -27,7 +23,8 @@ function rerun()
         }
         chrome.downloads.search({
             id: download.id,
-        }).then(x =>{
+        }).then(x =>
+        {
             if (x.length === 0)
             {
                 return;
